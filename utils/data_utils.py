b72df236d43708dca1576e1d6b5efb7d8daf6b4c
Uroboros0313 <ouroboros_csl@163.com>
2023-01-20 13:35:44 +0800

    add DataTable

--- utils/data_utils.py
+++ utils/data_utils.py
@@ -14,15 +14,92 @@ import numpy as np
 def load_data(data_dir:Path, dataset:str='criteo'):
     if not isinstance(data_dir, Path):
         data_dir = Path(data_dir)
-    
+        
     if dataset == 'criteo':
         train = pd.read_csv(data_dir / 'train.csv')
         test = pd.read_csv(data_dir / 'test_csv')
-        
+            
     elif dataset == 'ml-1m':
         pass
     elif dataset == 'ml-100k':
         pass
+    
+    return train, test
+    
+    
+class DataTable():
+    def __init__(
+        self,
+        train_set,
+        test_set=None,
+        metadata=None) -> None:
+        
+        self.train_set = train_set
+        self.test_set = test_set
+        
+        if metadata == None:
+            self.__infer_metadata(train_set, )
+        else:
+            self.metadata = metadata
+    
+    def preprocess_data(
+        self,
+        num_scale='minmax',
+        cat_enc='label',
+        num_fill_method='mean',
+        cat_fill_val='<UNKNOWN>'):
+        '''
+        1.缺失值填充
+        2.标准化
+        3.标签编码
+        4.补充metadata
+        '''
+        
+    def __fill_null(self):
+        pass
+    
+    def __encode(self):
+        pass
+    
+    @staticmethod
+    def label_encoder(series):
+        ss_vals = series.unique()
+        ss_lbs = list(range(len(ss_vals)))
+        val2lb = dict(zip(ss_vals, ss_lbs))
+        lb2val = dict(zip(ss_lbs, ss_vals))
+        
+        series = series.map(val2lb)
+        return series, val2lb, lb2val
+        
+    @staticmethod
+    def minmax_scaler(series):
+        ss_min = series.min()
+        ss_max = series.max()
+        series = (series - ss_min) / (ss_max - ss_min)
+        return series, ss_min, ss_max
+    
+    @staticmethod
+    def standard_scaler(series):
+        ss_mean = series.mean()
+        ss_std = series.std()
+        series = (series - ss_mean) / ss_std
+        return series, ss_mean, ss_std
+
+    
+    def __infer_metadata(self, df:pd.DataFrame, dataset=None)->Dict[str, List]:
+        '''
+        区分类别型数据与数值型数据
+        '''
+        num_cols = df.select_dtypes(include='number').columns.to_list()
+        cat_cols = df.select_dtypes(include=object).columns.to_list()
+            
+        for n_col in num_cols:
+            if df[n_col].nunique() < len(df) * 0.01:
+                num_cols.remove(n_col)
+                cat_cols.append(cat_cols)
+        
+        self.metadata = {'num_cols': num_cols, 'cat_cols': cat_cols}
+        
 
 
 def manual_feature_cross(df:pd.DataFrame, cross_pairs:List=[])->pd.DataFrame:
@@ -42,41 +119,8 @@ def manual_feature_cross(df:pd.DataFrame, cross_pairs:List=[])->pd.DataFrame:
     
     return df
 
+    
 
-def preprocess_data():
-    '''
-    1.缺失值填充
-    2.标准化
-    3.标签编码
-    4.补充metadata
-    '''
-    pass
-
-    
-def _infer_metadata(df:pd.DataFrame, dataset=None)->Dict[str, List]:
-    '''
-    区分类别型数据与数值型数据
-    '''
-    if dataset == None:
-        num_cols = df.select_dtypes(include='number').columns.to_list()
-        cat_cols = df.select_dtypes(include=object).columns.to_list()
-        
-        for n_col in num_cols:
-            if df[n_col].nunique() < len(df) * 0.05:
-                num_cols.remove(n_col)
-                cat_cols.append(cat_cols)
-                
-    elif dataset == 'criteo':
-        cols = df.columns
-        num_cols = cols[df.columns.str.startswith('I')].to_list()
-        cat_cols = cols[df.columns.str.startswith('C')].to_list()
-        
-    else:
-        pass
-    
-    metadata = {'num_cols': num_cols, 'cat_cols': cat_cols}
-    
-    return metadata
     
 class DataCenter():
     pass
