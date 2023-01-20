934e6e3dc10ce3d0aa9c5938ddb3640616882a5d
Uroboros0313 <ouroboros_csl@163.com>
2023-01-20 08:45:34 +0800

    add manual feature cross

--- utils/data_utils.py
+++ utils/data_utils.py
@@ -4,7 +4,9 @@
 #@Author  :   Li Suchi 
 #@Email   :   lsuchi@126.com
 
+from typing import List, Dict
 from pathlib import Path
+from itertools import product
 
 import pandas as pd
 import numpy as np
@@ -22,11 +24,32 @@ def load_data(data_dir:Path, dataset:str='criteo'):
     elif dataset == 'ml-100k':
         pass
 
+
+def manual_feature_cross(df:pd.DataFrame, cross_pairs:List=[])->pd.DataFrame:
+    '''
+    WideDeep论文中的手动特征交叉
+    '''
+    for c1_name, c2_name in cross_pairs:
+        ss1, ss2 = df[c1_name], df[c2_name]
+        c1 = ss1.unique()
+        c2 = ss2.unique()
+        
+        combs = list(product(c1, c2))
+        combs_id = range(len(combs))
+        map_ = dict(zip(combs, combs_id))
+        ss_comb = pd.Series(zip(ss1, ss2)).map(map_)
+        df = pd.concat([df, pd.get_dummies(ss_comb, prefix=f"CP_{c1_name}_{c2_name}")], axis=1)
+    
+    return df
+
+
 def preprocess_data():
     pass
+
     
 def _type_infer():
     pass
+
 
 class DataCenter():
     pass
